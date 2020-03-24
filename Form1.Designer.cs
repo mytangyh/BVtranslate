9b1dfa4f50b5fe305780787ebec3926a7d3cf95b
mytangyh <1450521339@qq.com>
2020-03-24 17:48:54 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BVtranslateAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AVtext = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BVtext = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BV：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AVtext
+            // 
+            this.AVtext.Location = new System.Drawing.Point(181, 167);
+            this.AVtext.Name = "AVtext";
+            this.AVtext.Size = new System.Drawing.Size(129, 25);
+            this.AVtext.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AV：";
+            // 
+            // BVtext
+            // 
+            this.BVtext.Location = new System.Drawing.Point(181, 96);
+            this.BVtext.Name = "BVtext";
+            this.BVtext.Size = new System.Drawing.Size(129, 25);
+            this.BVtext.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(462, 335);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(79, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "by:Evader";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 368);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.BVtext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AVtext);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BVtranslateAV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox AVtext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BVtext;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BVtranslateAV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       public int translate(String x)
+        {
+            int r=0;
+            Dictionary<char, int> dic= new Dictionary<char, int>();          
+            String table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+            for (int i=0;i<58;i++)
+            {
+                dic.Add(table[i],i);
+            }
+            int[] s = {11,10,3,8,4,6};
+            int xo = 177451812;
+            long add = 8728348608;
+            for (int i = 0; i < 6; i++)
+            {
+                
+                r+=dic[x[s[i]]]*(int)Math.Pow(58,i);
+
+            }
+            int re = (int)(r - add) ^ xo;
+            return re;
+;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String x=BVtext.Text.ToString();
+            if (x.Length < 12)
+            {
+                x = "BV17x411w7KC";
+                BVtext.Text = x;
+            }
+            int r = translate(x);
+            String re = r + "";
+            AVtext.Text = re;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://space.bilibili.com/40506842/video");
+        }
+    }
+}
